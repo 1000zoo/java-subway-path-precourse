c735dc5101d30ea35a1c38ba2ee7601f1cb7761b
1000zoo <cjswldn99@gmail.com>
2023-12-12 17:09:19 +0900

    feat(RepositoryStarter): createLineRepository 추가

--- src/main/java/subway/repository/RepositoryStarter.java
+++ src/main/java/subway/repository/RepositoryStarter.java
@@ -1,6 +1,9 @@
 package subway.repository;
 
 import java.util.List;
+import java.util.Map;
+import subway.domain.Line;
+import subway.domain.LineRepository;
 import subway.domain.Relationship;
 import subway.domain.RelationshipRepository;
 import subway.domain.Station;
@@ -14,7 +17,13 @@ public class RepositoryStarter {
             "교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"
     );
 
-    private final static List<List<String>> lineInformation = List.of(
+    private final static Map<String, List<String>> lineNames = Map.of(
+            "2호선", List.of("교대역", "강남역", "역삼역"),
+            "3호선", List.of("교대역", "남부터미널역", "양재역", "매봉역"),
+            "신분당선", List.of("강남역", "양재역", "양재시민의숲역")
+    );
+
+    private final static List<List<String>> informations = List.of(
             List.of("교대역", "강남역", "2", "3"),
             List.of("강남역", "역삼역", "2", "3"),    //2호선
 
@@ -35,6 +44,7 @@ public class RepositoryStarter {
         }
         init = true;
         createStationRepository();
+        createLineRepository();
         createRelationshipRepository();
     }
 
@@ -46,10 +56,21 @@ public class RepositoryStarter {
     }
 
     private static void createLineRepository() {
+        for (Map.Entry<String, List<String>> entry : lineNames.entrySet()) {
+            Line line = new Line(entry.getKey());
+            lineAdder(line, entry.getValue());
+            LineRepository.addLine(line);
+        }
+    }
+
+    private static void lineAdder(Line line, List<String> stationNames) {
+        for (String name : stationNames) {
+            line.addStation(name);
+        }
     }
 
     private static void createRelationshipRepository() {
-        for (List<String> information : lineInformation) {
+        for (List<String> information : informations) {
             Relationship relationship = Relationship.from(information);
             RelationshipRepository.addRelationship(relationship);
         }
