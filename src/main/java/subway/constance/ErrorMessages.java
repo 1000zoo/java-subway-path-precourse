11b7f03ca7f4573379a9ad4a3cef533e3320179a
1000zoo <cjswldn99@gmail.com>
2023-12-12 18:27:49 +0900

    feat(constance): 출력문구 상수 Enum 생성

---
+++ src/main/java/subway/constance/ErrorMessages.java
@@ -0,0 +1,17 @@
+package subway.constance;
+
+public enum ErrorMessages {
+    INVALID_INPUT("유효하지 않은 입력입니다."),
+    SAME_STATION("출발역과 도착역이 동일합니다."),
+    UNCONNECTED("출발역과 도착역이 연결되어있지 않습니다.");
+
+    private final String message;
+
+    ErrorMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
