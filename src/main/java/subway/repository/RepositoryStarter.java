f05802a576d4a5744a36ca874d7f7318a1e892bd
1000zoo <cjswldn99@gmail.com>
2023-12-12 16:21:08 +0900

    feat(RepositoryStarter): Repository 초기화 클래스

---
+++ src/main/java/subway/repository/RepositoryStarter.java
@@ -0,0 +1,39 @@
+package subway.repository;
+
+import java.util.List;
+import subway.domain.Station;
+import subway.domain.StationRepository;
+
+public class RepositoryStarter {
+
+    private final List<String> stationNames = List.of(
+            "교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"
+    );
+    private final List<List<String>> lineInformation = List.of(
+            List.of("교대역", "강남역", "2", "3"),
+            List.of("강남역", "역삼역", "2", "3"),    //2호선
+
+            List.of("교대역", "남부터미널역", "3", "2"),
+            List.of("남부터미널역", "양재역", "6", "5"),
+            List.of("양재역", "매봉역", "1", "1"),    //3호선
+
+            List.of("강남역", "양재역", "2", "8"),
+            List.of("양재역", "양재시민의숲역", "10", "3")    //신분당선
+    );
+
+    public RepositoryStarter() {
+        createStationRepository();
+        createLineRepository();
+    }
+
+    private void createStationRepository() {
+        for (String name : stationNames) {
+            Station station = new Station(name);
+            StationRepository.addStation(station);
+        }
+    }
+
+    private void createLineRepository() {
+
+    }
+}
